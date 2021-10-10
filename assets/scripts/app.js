c2c975c9cd23138561784e4382812602a0f06d2e
Gauthaman Sahadevan <gsahadevan@outlook.com>
2021-10-10 04:54:56 +0200

    Auto-generated commit [ci skip]

---
+++ assets/scripts/app.js
@@ -0,0 +1,22 @@
+// document.addEventListener('DOMContentLoaded', function () {
+// 	a('.sidebar-toggle').on('click touch', function () {
+// 		alert('hi');
+// 		a('.sidebar').toggleClass('toggled');
+// 	});
+// var t = a('.sidebar .active');
+// if (t.length && t.parent('.collapse').length) {
+// 	var e = t.parent('.collapse');
+// 	e.prev('a').attr('aria-expanded', !0), e.addClass('show');
+// }
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+// 	const sidebarToggle = document.getElementById('sidebar-toggle');
+// 	console.log(sidebarToggle);
+// 	if (sidebarToggle) {
+// 		sidebarToggle.addEventListener('click touch', function () {
+// 			const sidebar = document.getElementById('sidebar');
+// 			sidebar.classList.toggle('toggled');
+// 		});
+// 	}
+// });
